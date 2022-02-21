7feb366e447022c0c3377fb2301b54a60347f205
MylleneCouto <86529996+MylleneCouto@users.noreply.github.com>
2022-02-21 14:45:55 -0300

    att

---
+++ assets/js/main.js
@@ -0,0 +1,8 @@
+function menu(){
+
+  const menu = document.querySelector(".toggle");
+  const section = document.querySelector("section")
+
+  menu.classList.toggle("ativo");
+  section.classList.toggle("ativo")
+}
